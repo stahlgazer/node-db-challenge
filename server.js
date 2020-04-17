003711c84518cb4f3d83eda7f631a1d4dca50087
gavin stahl <stahlgavin@gmail.com>
2020-04-17 09:25:55 -0700

    database/tables finished

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
-
 // const projectsRouter = require("./projects/projectsRouter.js");
 // const tasksRouter = require("./tasks/tasksRouter.js");
 // const resourcesRouter = require("./resources/resourcesRouter.js");
-
 const server = express();
-
 server.use(express.json());
 
 // server.use("/api/projects", projectsRouter);
